ba907e2bc851a1272e85402810e10b3a652f4aff
Chandrakala A <Chandrakala.A@Ltts.com>
2021-04-13 17:47:18 +0530

    contosoProj

---
+++ source/repos/ContasoCraftsWebSite/ContasoCraftsWebSite/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using ContasoCraftsWebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContasoCraftsWebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+        JsonFileProductService ProductService;
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            ProductService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
